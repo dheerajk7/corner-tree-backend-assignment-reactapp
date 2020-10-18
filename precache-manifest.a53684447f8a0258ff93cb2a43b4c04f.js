c20bc158e8c8fb37973ccfb0fd7239e81f82c965
Dheeraj Kushwah <dheeraj77253@gmail.com>
2020-10-18 11:22:41 +0530

    Updates

---
+++ precache-manifest.a53684447f8a0258ff93cb2a43b4c04f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dc6530107778c0848fe81711ea3cfce",
+    "url": "/corner-tree-backend-assignment-reactapp/index.html"
+  },
+  {
+    "revision": "b72456d617806b222488",
+    "url": "/corner-tree-backend-assignment-reactapp/static/css/main.85d3df81.chunk.css"
+  },
+  {
+    "revision": "2b8c24f2fb452c7cc3b3",
+    "url": "/corner-tree-backend-assignment-reactapp/static/js/2.d05cd725.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/corner-tree-backend-assignment-reactapp/static/js/2.d05cd725.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b72456d617806b222488",
+    "url": "/corner-tree-backend-assignment-reactapp/static/js/main.d05c4573.chunk.js"
+  },
+  {
+    "revision": "067fce96d751a88381fc",
+    "url": "/corner-tree-backend-assignment-reactapp/static/js/runtime-main.501adcb5.js"
+  }
+]);
